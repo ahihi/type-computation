15ba8d672368905c2f3763abe918d33f5c8f36a7
ahihi <violetmaze@gmail.com>
2012-11-10 20:44:00 +0200

    natural + * ^, integer + - * ^

---
+++ Arithmetic.hs
@@ -0,0 +1,60 @@
+{-# LANGUAGE TypeFamilies, UndecidableInstances #-}
+
+module Arithmetic where
+
+-- Natural numbers
+data Z   -- Zero
+data S n -- Successor of n (n = Z or n = S n')
+
+type N0 = Z
+type N1 = S N0
+
+-- Natural addition
+type family NAdd m n
+type instance NAdd m Z      = m
+type instance NAdd m (S n') = NAdd (S m) n'
+
+-- Natural multiplication
+type family NMul m n
+type instance NMul m Z      = N0
+type instance NMul m (S n') = NAdd m (NMul m n')
+
+-- Natural power
+type family NPow m n
+type instance NPow m Z      = N1
+type instance NPow m (S n') = NMul m (NPow m n')
+
+-- Integers
+data I a b -- Integers of the form a-b (a and b are natural numbers)
+
+type I0 = I N0 N0
+type I1 = I N1 N0
+
+-- Integer normalization
+type family INorm n
+type instance INorm (I a Z)           = I a Z
+type instance INorm (I Z b)           = I Z b
+type instance INorm (I (S a') (S b')) = I a' b'
+
+-- Integer addition
+type family IAdd m n
+type instance IAdd (I am bm) (I an bn)
+    = INorm (I (NAdd am an) (NAdd bm bn))
+
+-- Integer negation
+type family INeg n
+type instance INeg (I a b) = INorm (I b a)
+
+-- Integer subtraction
+type family ISub m n
+type instance ISub m n = IAdd m (INeg n)
+
+-- Integer multiplication
+type family IMul m n
+type instance IMul (I am bm) (I an bn)
+    = INorm (I (NAdd (NMul am an) (NMul bm bn)) (NAdd (NMul am bn) (NMul bm an)))
+
+-- Integer power (with natural exponent)
+type family IPow m n
+type instance IPow m Z      = I1
+type instance IPow m (S n') = IMul m (IPow m n')
