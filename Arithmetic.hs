d37cc060620cc2386ed2c72a7a9bb1e83b5fb669
ahihi <violetmaze@gmail.com>
2012-11-10 22:04:32 +0200

    natural/integer division

--- Arithmetic.hs
+++ Arithmetic.hs
@@ -6,11 +6,13 @@ import Boolean
     ( False
     , True
     , And
+    , If
     )
 import Ordering
     ( LT
     , EQ
     , GT
+    , OEq
     )
 
 -- Natural numbers
@@ -33,10 +35,23 @@ type family NAdd m n
 type instance NAdd m Z      = m
 type instance NAdd m (S n') = NAdd (S m) n'
 
+-- Natural subtraction
+type family NSub m n
+type instance NSub m Z           = m
+type instance NSub Z Z           = Z
+type instance NSub (S m') (S n') = NSub m' n'
+
 -- Natural multiplication
 type family NMul m n
 type instance NMul m Z      = N0
 type instance NMul m (S n') = NAdd m (NMul m n')
+
+-- Natural division
+type family NDiv m n
+type instance NDiv Z (S n')      = Z
+type instance NDiv (S m') (S n') = If (OEq LT (NCmp m' n'))
+                                      Z
+                                      (S (NDiv (NSub m' n') (S n')))
 
 -- Natural power
 type family NPow m n
@@ -53,7 +68,11 @@ type I2 = I N2 N0
 
 -- Integer from natural
 type family IFromN n
-type instance IFromN n = I n I0
+type instance IFromN n = I n N0
+
+-- Integer to natural
+type family NFromI n
+type instance NFromI (I m Z) = m
 
 -- Integer comparison
 type family ICmp m n
@@ -64,6 +83,18 @@ type family INorm n
 type instance INorm (I a Z)           = I a Z
 type instance INorm (I Z b)           = I Z b
 type instance INorm (I (S a') (S b')) = I a' b'
+
+-- Integer signum
+type family ISign n
+type instance ISign n = If (OEq LT (ICmp n I0))
+                           I_1
+                           (If (OEq GT (ICmp n I0))
+                               I1
+                               I0)
+
+-- Integer absolute value
+type family IAbs n
+type instance IAbs n = IMul n (ISign n)
 
 -- Integer addition
 type family IAdd m n
@@ -82,6 +113,11 @@ type instance ISub m n = IAdd m (INeg n)
 type family IMul m n
 type instance IMul (I am bm) (I an bn)
     = INorm (I (NAdd (NMul am an) (NMul bm bn)) (NAdd (NMul am bn) (NMul bm an)))
+
+-- Integer division
+type family IDiv m n
+type instance IDiv m n
+    = IMul (IFromN (NDiv (NFromI (IAbs m)) (NFromI (IAbs n)))) (IMul (ISign m) (ISign n))
 
 -- Integer power (with natural exponent)
 type family IPow m n
